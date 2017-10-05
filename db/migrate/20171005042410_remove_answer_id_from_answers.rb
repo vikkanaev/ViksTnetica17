1c2a685288d4b255b3c4e45e1691e72bcbdb6a8b
Viktor Kanaev <vikkanaev@gmail.com>
2017-10-05 07:28:38 +0300

    Revert "Delete wrong columns from migrations"
    This reverts commit 0b1a2c772b4530ebc4e78db8c6a55f23c1f9d51e.

--- db/migrate/20171005042410_remove_answer_id_from_answers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAnswerIdFromAnswers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :answers, :answer_id, :integer
-  end
-end
--- db/migrate/20171005042526_remove_question_id_from_question.rb
+++
@@ -1,5 +0,0 @@
-class RemoveQuestionIdFromQuestion < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :questions, :question_id, :integer
-  end
-end
