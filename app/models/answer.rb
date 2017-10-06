710687c7feeb348dec1ac3eff1821ea847c9dab0
Viktor Kanaev <vikkanaev@gmail.com>
2017-10-06 16:33:20 +0300

    Make association changes

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,4 @@
 class Answer < ApplicationRecord
-  belongs_to :question, dependent: :destroy
-  validates :title, :body, :question_id, presence: true
+  belongs_to :question
+  validates :title, :body, presence: true
 end
