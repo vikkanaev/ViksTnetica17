e60f9485a645ced658bb64a50706c509d8c8fa4d
vikkanaev <vikkanaev@gmail.com>
2017-10-09 00:06:41 +0300

    Revert "Revert "Lesson4 controllers testing""
    This reverts commit 5e47d8b54ee46f7c9a48c52d7762668f2c534164.

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,21 @@
+class AnswersController < ApplicationController
+  before_action :find_question, only: [:create]
+  def create
+    @answer = @question.answers.new(answer_params)
+    if @answer.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:title, :body)
+  end
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+end
