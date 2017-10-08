044db89bde7aa741c3e8a00fe59b3e016505275e
vikkanaev <vikkanaev@gmail.com>
2017-10-09 00:06:41 +0300

    Revert "Lesson4 controllers testing"

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :questions  do
-    resources :answers, except: [:update, :destroy]
-  end
-  resources :answers, only: [:update, :destroy]
 end
