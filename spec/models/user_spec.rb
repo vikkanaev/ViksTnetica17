50b11b7b9f83fd22abafbf9b2cee9c1e0d208620
Viktor Kanaev <vikkanaev@gmail.com>
2017-10-20 09:00:40 +0300

    Fixes part 7.1

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe User, type: :model do
     let!(:user_not_author) { create(:user) }
     let!(:question) { create(:question, user: user_author) }
 
-    context 'model should response to author_of?' do
-      it { expect(user_author).to respond_to(:author_of?) }
-    end
     context 'Return True if user is author' do
       it { expect(user_author.author_of?(question)).to be(true) }
+    end
+    context 'Return False if user is not author' do
+      it { expect(user_not_author.author_of?(question)).to be(false) }
     end
   end
 end
