484b5b9f44efd64805607eb97cecc7e3a6858932
Viktor Kanaev <vikkanaev@gmail.com>
2017-11-20 23:13:09 +0300

    Made Votable model

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -4,4 +4,20 @@ module Votable
   included do
     has_many :votes, as: :votable, dependent: :destroy
   end
+
+  def vote_up(user)
+    votes.create(score: 1, user: user)
+  end
+
+  def vote_down(user)
+    votes.create(score: -1, user: user)
+  end
+
+  def vote_cancel(user)
+    votes.find_by(user: user).destroy if votes.find_by(user: user)
+  end
+
+  def sum_score
+    votes.sum(:score)
+  end
 end
--- spec/models/concerns/votable.rb
+++ spec/models/concerns/votable.rb
@@ -6,18 +6,32 @@ shared_examples_for 'votable' do
   let(:votable) { create(model.to_s.underscore.to_sym) }
 
   describe '#vote_up' do
-    it 'voiting up'
+    it 'Create new votable entry with value 1' do
+      expect { votable.vote_up(user) }.to change(votable.votes, :count).by(1)
+      expect( votable.votes.last.score ).to eq(1)
+    end
   end
 
   describe '#vote_down' do
-    it 'voiting down'
+    it 'Create new votable entry with value -1' do
+      expect { votable.vote_down(user) }.to change(votable.votes, :count).by(1)
+      expect( votable.votes.last.score ).to eq(-1)
+    end
   end
 
   describe '#vote_cancel' do
-    it 'vote_cancel'
+    before { votable.vote_up(user) }
+    it 'Destroy votable entry if exists' do
+      expect { votable.vote_cancel(user) }.to change(votable.votes, :count).by(-1)
+    end
   end
 
-  describe '#sum_value' do
-    it 'sum_value'
+  describe '#sum_score' do
+    let!(:vote_up) { create_list(:vote, 5, :up, votable: votable) }
+    let!(:vote_down) { create_list(:vote, 3, :down, votable: votable) }
+
+    it 'Retern sum of all votable scores' do
+      expect( votable.sum_score ).to eq(2)
+    end
   end
 end
