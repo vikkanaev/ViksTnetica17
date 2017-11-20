2fc8852d01880a15b56cc1f706edfa6aed26e4ae
Viktor Kanaev <vikkanaev@gmail.com>
2017-11-20 21:52:59 +0300

    Create Vote concern and basic test

---
+++ app/models/concerns/votable.rb
@@ -0,0 +1,7 @@
+module Votable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :votes, as: :votable, dependent: :destroy
+  end
+end
---
+++ spec/acceptance/questions/6_vote_for_question_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../acceptance_helper'
+
+feature 'Authenticated user vote for question', %q{
+  In order to find best question
+  As an User
+  I want to be able to vote for questions
+} do
+
+  scenario 'Authenticated user vote for question' do
+    sign_in(user)
+    visit questions_path
+    click_on 'Vote for Question'
+
+    expect(page).to have_content 'Your vote was counted'
+  end
+end
