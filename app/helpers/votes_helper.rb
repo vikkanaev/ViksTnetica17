328deecd5b63a5420e1c02f57c7459ad8d315260
Viktor Kanaev <vikkanaev@gmail.com>
2017-11-24 15:53:51 +0300

    Add helper for vote buttons

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,2 +1,9 @@
 module VotesHelper
+  def vote_btn_show?(votable)
+    return votable.votes.exists? ? 'no_display_btn' : ''
+  end
+
+  def vote_cancel_show?(votable)
+    return votable.votes.exists? ? '' : 'no_display_btn'
+  end
 end
