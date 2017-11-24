1d43d782269d8ec61195b255b34d8c30069f9d30
Viktor Kanaev <vikkanaev@gmail.com>
2017-11-24 10:02:36 +0300

    Add acceptance tests for answers vote and fix all other tests

--- spec/acceptance/questions/5_delete_attached_file_from_queestion_spec.rb
+++ spec/acceptance/questions/5_delete_attached_file_from_queestion_spec.rb
@@ -27,7 +27,7 @@ feature 'Authenticated user delete files on his question', %q{
     within '#attachment-1' do
       click_on 'Delete'
     end
-    wait_for_ajax
+    #wait_for_ajax
     within '.question' do
       expect(page).to_not have_link 'README.md', href: '/uploads/attachment/file/1/README.md'
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
