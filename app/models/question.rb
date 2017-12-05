b0ef443417459fb3cab956d1a88b36d3e3e1ac5e
Viktor Kanaev <vikkanaev@gmail.com>
2017-12-05 09:34:46 +0300

    Create polymorphic Comment model and relations 4 it

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachmentable
+  has_many :comments, as: :commentable, dependent: :destroy
 
   validates :title, :body, presence: true
   accepts_nested_attributes_for :attachments, reject_if: :all_blank
