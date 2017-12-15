7b448ff40581e284159ac326126a7ced745c26c9
Viktor Kanaev <vikkanaev@gmail.com>
2017-12-15 20:03:11 +0300

    Make Facebook oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ApplicationRecord
   has_many :questions, dependent: :destroy
-  has_many :answer, dependent: :destroy
+  has_many :answers, dependent: :destroy
+  has_many :authorizations
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
 
   def author_of?(object)
     object.user_id == id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    unless user
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+    end
+    user.authorizations.create(provider: auth.provider, uid: auth.uid) if user
+    user
   end
 end
---
+++ spec/factories/authorizations.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :authorization do
+    user ""
+    provider ""
+    uid "MyString"
+  end
+end
