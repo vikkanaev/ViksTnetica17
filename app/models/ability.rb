be4cf15f1661e047db0a2d29c650a3504f9ef747
Viktor Kanaev <vikkanaev@gmail.com>
2018-01-26 15:35:06 +0300

    Make vote_cancel and tests for it

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -21,26 +21,27 @@ class Ability
     can :manage, :all
   end
 
-  def user_abilities
+  def user_abilities # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
     guest_abilities
     can :create, [Question, Answer, Comment, Attachment]
-    can [:vote_up, :vote_down], Answer do |votable|
+    can :update, [Question, Answer, Comment, Attachment], user_id: user.id
+
+    can [:vote_up, :vote_down], [Answer, Question] do |votable|
       votable.user_id != user.id
     end
 
-    can :vote_cancel, Vote, user_id: user.id
+    can :vote_cancel, [Answer, Question] do |votable|
+      votable.votes.where(user_id: user.id).any?
+    end
 
     can :set_best_answer_ever, Answer do |answer|
       answer.question.user_id == user.id
     end
 
-    can :update, [Question, Answer, Comment, Attachment], user_id: user.id
-    can :destroy, [Question, Answer, Comment], user_id: user.id
+    can :destroy, [Question, Answer, Comment, Vote], user_id: user.id
 
     can :destroy, Attachment do |att|
       att.attachmentable.user_id == user.id
     end
-
-    can :destroy, Vote, user_id: user.id
   end
 end
