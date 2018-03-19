4c5522b49eed3c687fdc33337919cbc3851aea91
Viktor Kanaev <vikkanaev@gmail.com>
2018-03-19 11:55:14 +0300

    Make tests

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Search, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'method search_result' do
+    %w[Noname Everywhere ''].each do |attr|
+      it "gets condition #{attr}" do
+        expect(ThinkingSphinx).to receive(:search).with("ask", {:star=>true})
+        Search.search_result('ask', attr)
+      end
+    end
+    %w[Questions Answers Comments Users].each do |attr|
+      it "gets condition: #{attr}" do
+        expect(attr.singularize.classify.constantize).to receive(:search).with("ask", {:star=>true})
+        Search.search_result('ask', attr)
+      end
+    end
+  end
 end
