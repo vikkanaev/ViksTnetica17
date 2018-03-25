bd53e6c527db0d8b6fa274e1f48c2855a581105a
Viktor Kanaev <vikkanaev@gmail.com>
2018-03-25 13:11:46 +0300

    Make caching

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,8 @@ Rails.application.configure do
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
-    config.cache_store = :memory_store
+    # config.cache_store = :memory_store
+    config.cache_store = :redis_store, 'redis://localhost:6379', { expires_in: 90.minutes }
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
